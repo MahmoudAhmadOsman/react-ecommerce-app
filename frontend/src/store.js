8361a06ef7a90cd427921f3c2bf630bac3316cd9
Mahmoud Osman <mahmoudosman.som@gmail.com>
2020-12-25 11:19:08 -0600

    added redux store, reducer & initialState

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,7 +2,7 @@ import data from "./data";
 import { applyMiddleware, compose, createStore } from "redux";
 import thunk from "redux-thunk";
 
-//Steps: 1: InitialState, 2: reducer
+//Steps: 1: InitialState, 2: reducer 3: store
 const InitialState = {};
 
 //2: Reducer(param1, param2), ex. reducer(state, action)
@@ -10,10 +10,10 @@ const reducer = (state, action) => {
   return { products: data.products };
 };
 
-//Now add thunk for Google Chrome in order to see action & state changes
+//Now, add thunk for Google Chrome in order to see action & state changes
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-//3: create store, takes 2 params createStore(reducer, InitialState)
+//3: createStore function, takes 2 params createStore(reducer, InitialState) and  thunk middleware
 const store = createStore(
   reducer,
   InitialState,

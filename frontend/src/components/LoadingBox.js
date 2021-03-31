27346e2236e4a6a1569851a6c5c3498339856c04
Mahmoud Osman <mahmoudosman.som@gmail.com>
2021-03-31 14:39:10 -0500

    modified product.js & homescreen.js components

--- frontend/src/components/LoadingBox.js
+++ frontend/src/components/LoadingBox.js
@@ -2,8 +2,12 @@ import React from "react";
 
 export default function LoadingBox() {
   return (
-    <div>
-      <i className="fa fa-spinner fa-spin"></i> Loading...
+    <div className="container mt-4">
+      <div className="row">
+        <div className="col-md-6 mx-auto">
+          <i className="fa fa-spinner fa-spin"></i> Loading...
+        </div>
+      </div>
     </div>
   );
 }

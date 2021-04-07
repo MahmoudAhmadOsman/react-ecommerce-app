a2d061f8ba69aafcee5ab39d826252aa2fb9b0b8
Mahmoud Osman <mahmoudosman.som@gmail.com>
2021-04-06 22:38:47 -0500

    added models, routes & currency formatter function

--- backend/data.js
+++ backend/data.js
@@ -2,15 +2,15 @@ import bcrypt from "bcryptjs";
 const data = {
   users: [
     {
-      name: 'Mahmoud',
-      email: 'admin@example.com',
-      password: bcrypt.hashSync('123456789', 10),
+      name: "Mahmoud",
+      email: "admin@example.com",
+      password: bcrypt.hashSync("123456789", 10),
       isAdmin: true,
     },
     {
-      name: 'Osman',
-      email: 'user@gmail.com',
-      password: bcrypt.hashSync('1234', 8),
+      name: "Osman",
+      email: "user@gmail.com",
+      password: bcrypt.hashSync("123456789", 10),
       isAdmin: false,
     },
   ],

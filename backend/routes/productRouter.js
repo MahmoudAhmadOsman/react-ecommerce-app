ead8503e7ef3cba1d8ccef72a1ba0e9ab6cc3c33
Mahmoud Osman <mahmoudosman.som@gmail.com>
2021-04-14 08:08:23 -0500

    added product router

--- backend/routes/productRouter.js
+++ backend/routes/productRouter.js
@@ -3,15 +3,37 @@ import expressAsyncHandler from "express-async-handler";
 import data from "../data.js";
 import Product from "../models/productModel.js";
 
-const productRouter = express.Router;
+const productRouter = express.Router();
 
-//Define product seed api
+productRouter.get(
+  "/",
+  expressAsyncHandler(async (req, res) => {
+    const products = await Product.find({}); // Get all products from database
+    res.send(products); // Send the products to frontend
+  })
+);
 
 productRouter.get(
   "/seed",
   expressAsyncHandler(async (req, res) => {
+    // await Product.remove({}); // remove product in the database in order to create new products
     const createdProducts = await Product.insertMany(data.products);
     res.send({ createdProducts });
   })
 );
-export default ProductRouter;
+
+//Get a product by its id
+productRouter.get(
+  "/:id",
+  expressAsyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      // if product exists
+      res.send(product);
+    } else {
+      res.status(404).send({ message: "Product Not Found!" });
+    }
+  })
+);
+
+export default productRouter;
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express from "express";
 import mongoose from "mongoose";
+// import data from "./data.js";
 
-import ProductRouter from "./routes/productRouter.js";
 import userRouter from "./routes/userRouter.js";
+import productRouter from "./routes/productRouter.js";
 
 const app = express();
 
@@ -13,18 +14,13 @@ mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/mongodb2020", {
   useCreateIndex: true,
 });
 
-//Users router
+//Users api
 app.use("/api/users", userRouter);
-//Product router - dynamic data from backend
-app.use("/api/products", ProductRouter);
-
-//Products api from the backend or from data.js file
-app.get("api/products", (req, res) => {
-  res.send(data.products);
-});
+//Products api
+app.use("/api/products", productRouter);
 
 app.get("/", (req, res) => {
-  res.send(`<h2>Great!. Server is running:  ${port}</h2>`);
+  res.send("Server is running");
 });
 
 //Show all errors - server errors
@@ -33,7 +29,12 @@ app.use((err, req, res, next) => {
 });
 
 const port = process.env.PORT || 5000;
-
 app.listen(port, () => {
-  console.log(`Server stated at http://localhost:${port}`);
+  console.log(`Server started at http://localhost:${port}`);
+});
+
+//Show/handle  server error in a better way
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Type of Error is : ${err}`);
+  server.close(() => process.exit(1));
 });

f1fce489c6a8d1edf90c743ec33b7b7a43b07b40
Mahmoud Osman <mahmoudosman.som@gmail.com>
2021-08-06 23:51:01 -0500

    added create order api express backen method

--- backend/routes/orderRouter.js
+++ backend/routes/orderRouter.js
@@ -36,7 +36,7 @@ const orderSchema = new mongoose.Schema(
     shippingPrice: { type: Number, required: true },
     taxPrice: { type: Number, required: true },
     totalPrice: { type: Number, required: true },
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, //Get the user who is creating the order
     isPaid: { type: Boolean, default: false },
     paidAt: { type: Date },
     isDelivered: { type: Boolean, default: false },

7de7ded21df353852aba7f8337c360e411096e2d
Mahmoud Osman <mahmoudosman.som@gmail.com>
2021-11-12 22:55:13 -0600

    fixed the spinning backend server error

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -5,10 +5,22 @@ const orderSchema = new mongoose.Schema(
 		//Order fields
 		orderItems: [
 			{
-				name: { type: String, required: true },
-				qty: { type: Number, required: true },
-				image: { type: String, required: true },
-				price: { type: Number, required: true },
+				name: {
+					type: String,
+					required: true,
+				},
+				qty: {
+					type: Number,
+					required: true,
+				},
+				image: {
+					type: String,
+					required: true,
+				},
+				price: {
+					type: Number,
+					required: true,
+				},
 				product: {
 					type: mongoose.Schema.Types.ObjectId,
 					ref: "Product",

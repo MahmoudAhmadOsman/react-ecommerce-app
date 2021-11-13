67b7a7df3944c287576e7e39e6e82220c2d101eb
Mahmoud Osman <mahmoudosman.som@gmail.com>
2021-11-12 22:53:30 -0600

    fixed the spinning backend server error

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 const orderSchema = new mongoose.Schema(
 	{
 		//Order fields
-
 		orderItems: [
 			{
 				name: { type: String, required: true },
@@ -21,25 +20,67 @@ const orderSchema = new mongoose.Schema(
 		//Shipping flieds
 
 		shippingAddress: {
-			fullName: { type: String, required: true },
-			address: { type: String, required: true },
-			city: { type: String, required: true },
-			postalCode: { type: String, required: true },
-			country: { type: String, required: true },
+			fullName: {
+				type: String,
+				required: true,
+			},
+			address: {
+				type: String,
+				required: true,
+			},
+			city: {
+				type: String,
+				required: true,
+			},
+			postalCode: {
+				type: String,
+				required: true,
+			},
+			country: {
+				type: String,
+				required: true,
+			},
 		},
 
 		//Payment method fields
 
-		paymentMethod: { type: String, required: true },
-		itemsPrice: { type: Number, required: true },
-		shippingPrice: { type: Number, required: true },
-		taxPrice: { type: Number, required: true },
-		totalPrice: { type: Number, required: true },
-		user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }, //Get the user who is creating the order using ref:
-		isPaid: { type: Boolean, default: false },
+		paymentMethod: {
+			type: String,
+			required: true,
+		},
+		itemsPrice: {
+			type: Number,
+			required: true,
+		},
+		shippingPrice: {
+			type: Number,
+			required: true,
+		},
+		taxPrice: {
+			type: Number,
+			required: true,
+		},
+		totalPrice: {
+			type: Number,
+			required: true,
+		},
+		user: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User",
+			required: true,
+		}, //Get the user who is creating the order using ref:
+		isPaid: {
+			type: Boolean,
+			default: false,
+		},
 		paidAt: { type: Date },
-		isDelivered: { type: Boolean, default: false },
-		deliveredAt: { type: Date },
+		isDelivered: {
+			type: Boolean,
+			default: false,
+		},
+		deliveredAt: {
+			type: Date,
+		},
 	},
 	{
 		timestamps: true,

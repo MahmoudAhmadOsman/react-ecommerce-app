5357a2a0544588eade7e88f4311d6e5bdf9b45af
Mahmoud Osman <mahmoudosman.som@gmail.com>
2021-11-19 23:09:29 -0600

    pushed to heroku

--- backend/server.js
+++ backend/server.js
@@ -38,6 +38,12 @@ app.use((err, req, res, next) => {
 	res.status(500).send({ message: err.message });
 });
 
+//Serve React build files in production
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.get("*", (req, res) =>
+	res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
+);
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
 	console.log(`Server started at http://localhost:${port}`);

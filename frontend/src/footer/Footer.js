3100ff7b48024984666ed9352589821fd56222e9
Mahmoud Osman <mahmoudosman.som@gmail.com>
2021-12-01 10:00:33 -0600

    modified footer date

--- frontend/src/footer/Footer.js
+++ frontend/src/footer/Footer.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 export default function Footer() {
+  const currentYear = new Date();
   return (
-    <footer className="footer">
-      <p>
-        &copy; Copyright. MAO Store { new Date().getFullYear() }. All rights reserved. Developed by
-        <a href="http://mahmoudosman.com"> Mahmoud Osman</a>
-      </p>
-    </footer>
-  );
+		<footer className="footer">
+			<p>
+				&copy; Copyright. MAO Store {currentYear.getFullYear()}. All rights
+				reserved. Designed & developed by
+				<a href="http://mahmoudosman.com"> Mahmoud Osman</a>.
+			</p>
+		</footer>
+	);
 }

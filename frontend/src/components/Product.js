cb36aade74f4ce5a881078506a415bd7b976bac7
Mahmoud Osman <mahmoudosman.som@gmail.com>
2021-12-09 08:16:58 -0600

    added react reveal animation library

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,60 +1,61 @@
 import React from "react";
+import { Zoom } from "react-reveal";
 import { Link } from "react-router-dom";
 import currencyFormat from "../utils/util";
 import Rating from "./Rating";
 const Product = (props) => {
-  const productListTitle = "All Products";
+	const productListTitle = "All Products";
 
+	const { products } = props;
+	return (
+		<section className="product-main-container mt-4">
+			<div className="container">
+				<h1 className="text-danger">{productListTitle}</h1> <hr />
+				<div className="row">
+					{products.map((product) => (
+						<div className="col-md-4 mb-3" key={product._id}>
+							<div className="card">
+								<Zoom>
+									<Link to={`/product/${product._id}`}>
+										<img
+											className="img-fluid"
+											src={product.image}
+											alt={product.name}
+											draggable="false"
+										/>
+									</Link>
+								</Zoom>
 
-  
-  const { products } = props;
-  return (
-    <section className="product-main-container mt-4">
-      <div className="container">
-        <h1 className="text-danger">{productListTitle}</h1> <hr />
-        <div className="row">
-          {products.map((product) => (
-            <div className="col-md-4 mb-3" key={product._id}>
-              <div className="card">
-                <Link to={`/product/${product._id}`}>
-                  <img
-                    className="img-fluid"
-                    src={product.image}
-                    alt={product.name}
-                    draggable="false"
-                  />
-                </Link>
+								<div className="card-body">
+									<h4 className="card-title">{product.name}</h4>
+									<p className="brand-name">
+										Brand: <span className="text-muted">{product.brand}</span>
+									</p>
+									<hr />
+									<p className="card-text">
+										<Rating
+											rating={product.rating}
+											numReviews={product.numReviews}
+										></Rating>
+									</p>
+									<button className="btn btn-outline-dark disabled">
+										{currencyFormat(product.price)}
+									</button>
 
-                <div className="card-body">
-                  <h4 className="card-title">{product.name}</h4>
-                  <p className="brand-name">
-                    Brand: <span className="text-muted">{product.brand}</span>
-                  </p>
-                  <hr />
-                  <p className="card-text">
-                    <Rating
-                      rating={product.rating}
-                      numReviews={product.numReviews}
-                    ></Rating>
-                  </p>
-                  <button className="btn btn-outline-dark disabled">
-                    {currencyFormat(product.price)}
-                  </button>
-
-                  <Link
-                    className="btn btn-outline-danger ml-1"
-                    to={`/product/${product._id}`}
-                  >
-                    View
-                  </Link>
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-    </section>
-  );
+									<Link
+										className="btn btn-outline-danger ml-1"
+										to={`/product/${product._id}`}
+									>
+										View
+									</Link>
+								</div>
+							</div>
+						</div>
+					))}
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default Product;
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { Zoom, Fade, Flip } from "react-reveal";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { detailsProduct } from "../actions/productActions";
@@ -10,96 +11,107 @@ import currencyFormat from "../utils/util";
 //import data from "../data";
 
 const ProductScreen = (props) => {
-  const dispatch = useDispatch();
-  //Find product by its id
-  // const product = data.products.find((x) => x._id === props.match.params.id); //old, static
-  //Get the product list from Redux store instead of getting details from static file
+	const dispatch = useDispatch();
+	//Find product by its id
+	// const product = data.products.find((x) => x._id === props.match.params.id); //old, static
+	//Get the product list from Redux store instead of getting details from static file
 
-  const productId = props.match.params.id;
-  //Set Quality
-  const [qty, setQty] = useState(1);
+	const productId = props.match.params.id;
+	//Set Quality
+	const [qty, setQty] = useState(1);
 
-  const productDetails = useSelector((state) => state.productDetails);
-  const { loading, error, product } = productDetails;
-  //If product doesn't exist
-  // if (!product) {
-  //   return <div className="product_not_found"> Product Not Found</div>;
-  // }
+	const productDetails = useSelector((state) => state.productDetails);
+	const { loading, error, product } = productDetails;
+	//If product doesn't exist
+	// if (!product) {
+	//   return <div className="product_not_found"> Product Not Found</div>;
+	// }
 
-  //lastly use useEffect()
+	//lastly use useEffect()
 
-  useEffect(() => {
-    dispatch(detailsProduct(productId));
-  }, [dispatch, productId]);
+	useEffect(() => {
+		dispatch(detailsProduct(productId));
+	}, [dispatch, productId]);
 
-  //Add to Cart button action
-  const addToCartHandler = () => {
-    props.history.push(`/cart/${productId}?qty=${qty}`);
-  };
+	//Add to Cart button action
+	const addToCartHandler = () => {
+		props.history.push(`/cart/${productId}?qty=${qty}`);
+	};
 
-  return (
-    <section className="product-details-page">
-      {loading ? (
-        <LoadingBox></LoadingBox>
-      ) : error ? (
-        <MessageBox variant="danger" className="text-center">
-          {error}
-        </MessageBox>
-      ) : (
-        <div className="product-container">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-12">
-                <Link to="/">
-                  <i
-                    className="fa fa-chevron-left mt-3"
-                    aria-hidden="true"
-                    title="Go back"
-                  ></i>
-                </Link>{" "}
-                <hr />
-              </div>
-            </div>
-          </div>
-          <div className="container">
-            <div className="row">
-              <div className="col-md-6">
-                <img
-                  className="img-fluid img-thumbnail"
-                  src={product.image}
-                  alt={product.name}
-                  draggable="false"
-                />
-                <h1>{product.name}</h1>
-                <p className="brand-name">
-                  Brand: <span className="text-muted">{product.brand}</span>
-                </p>
-                <hr />
-                <Rating
-                  rating={product.rating}
-                  numReviews={product.numReviews}
-                ></Rating>
-                <p className="product-title">
-                  <b> Product Details:</b>
-                </p>
-                <p>
-                  <p className="product-description">{product.description}</p>
-                </p>
-              </div>
-              <div className="col-md-3">
-                <h1>
-                  <b className="text-danger">{currencyFormat(product.price)}</b>
-                </h1>
-                <p>
-                  <b className="text-info">Status: </b>
-                  {product.countInStock > 0 ? (
-                    <small className="text-muted">In Stock</small>
-                  ) : (
-                    <small className="text-danger">Out Of Stock</small>
-                  )}
-                </p>
-              </div>
-              {/* <div className="col-md-2">
+	return (
+		<section className="product-details-page">
+			{loading ? (
+				<LoadingBox></LoadingBox>
+			) : error ? (
+				<MessageBox variant="danger" className="text-center">
+					{error}
+				</MessageBox>
+			) : (
+				<div className="product-container">
+					<div className="container">
+						<div className="row">
+							<div className="col-md-12">
+								<Link to="/">
+									<i
+										className="fa fa-chevron-left mt-3"
+										aria-hidden="true"
+										title="Go back"
+									></i>
+								</Link>{" "}
+								<hr />
+							</div>
+						</div>
+					</div>
+					<div className="container">
+						<div className="row">
+							<div className="col-md-6">
+								<Zoom>
+									<img
+										className="img-fluid img-thumbnail"
+										src={product.image}
+										alt={product.name}
+										draggable="false"
+									/>
+								</Zoom>
+
+								<Fade left>
+									<h1>{product.name}</h1>
+								</Fade>
+
+								<p className="brand-name">
+									Brand: <span className="text-muted">{product.brand}</span>
+								</p>
+								<hr />
+								<Rating
+									rating={product.rating}
+									numReviews={product.numReviews}
+								></Rating>
+								<p className="product-title">
+									<b> Product Details:</b>
+								</p>
+								<p>
+									<p className="product-description">{product.description}</p>
+								</p>
+							</div>
+							<div className="col-md-3">
+								<Flip left>
+									<h1>
+										<b className="text-danger">
+											{currencyFormat(product.price)}
+										</b>
+									</h1>
+								</Flip>
+
+								<p>
+									<b className="text-info">Status: </b>
+									{product.countInStock > 0 ? (
+										<small className="text-muted">In Stock</small>
+									) : (
+										<small className="text-danger">Out Of Stock</small>
+									)}
+								</p>
+							</div>
+							{/* <div className="col-md-2">
                 <h1 className="text-info">Status</h1>
                 {product.countInStock > 0 ? (
                   <small className="text-success">In Stock</small>
@@ -108,51 +120,51 @@ const ProductScreen = (props) => {
                 )}
               </div> */}
 
-              <div className="col-md-3">
-                <ul className="list-group">
-                  {product.countInStock > 0 && (
-                    <div
-                      style={{
-                        border: "1px solid #ccc",
-                        height: "290px",
-                        backgroundColor: "#fafafa",
-                        padding: "20px",
-                      }}
-                    >
-                      <div>
-                        <h1 className="text-info">Qty</h1>
-                        <div>
-                          <select
-                            className="form-control mb-5"
-                            value={qty}
-                            onChange={(e) => setQty(e.target.value)}
-                          >
-                            {[...Array(product.countInStock).keys()].map(
-                              (x) => (
-                                <option key={x + 1} value={x + 1}>
-                                  {x + 1}
-                                </option>
-                              )
-                            )}
-                          </select>
-                        </div>
-                      </div>
+							<div className="col-md-3">
+								<ul className="list-group">
+									{product.countInStock > 0 && (
+										<div
+											style={{
+												border: "1px solid #ccc",
+												height: "290px",
+												backgroundColor: "#fafafa",
+												padding: "20px",
+											}}
+										>
+											<div>
+												<h1 className="text-info">Qty</h1>
+												<div>
+													<select
+														className="form-control mb-5"
+														value={qty}
+														onChange={(e) => setQty(e.target.value)}
+													>
+														{[...Array(product.countInStock).keys()].map(
+															(x) => (
+																<option key={x + 1} value={x + 1}>
+																	{x + 1}
+																</option>
+															)
+														)}
+													</select>
+												</div>
+											</div>
 
-                      <button
-                        onClick={addToCartHandler}
-                        className="btn btn-outline-danger btn-block btn-lg mt-4"
-                      >
-                        <h3 className="font-weight-bold"> Add to Cart</h3>
-                      </button>
-                    </div>
-                  )}
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
-      )}
-    </section>
-  );
+											<button
+												onClick={addToCartHandler}
+												className="btn btn-outline-danger btn-block btn-lg mt-4"
+											>
+												<h3 className="font-weight-bold"> Add to Cart</h3>
+											</button>
+										</div>
+									)}
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			)}
+		</section>
+	);
 };
 export default ProductScreen;

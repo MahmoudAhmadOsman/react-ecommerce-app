27d79c4aaf219abbe1f2866c253e370ee200492f
Mahmoud Osman <mahmoudosman.som@gmail.com>
2021-12-09 08:30:15 -0600

    added react reveal animation library

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -5,18 +5,19 @@ import currencyFormat from "../utils/util";
 import Rating from "./Rating";
 const Product = (props) => {
 	const productListTitle = "All Products";
-
 	const { products } = props;
 	return (
 		<section className="product-main-container mt-4">
 			<div className="container">
-				<h1 className="text-danger">{productListTitle}</h1> <hr />
+				<h1 className="text-danger">{productListTitle}</h1>
+				<hr />
 				<div className="row">
 					{products.map((product) => (
 						<div className="col-md-4 mb-3" key={product._id}>
 							<div className="card">
 								<Zoom>
 									<Link to={`/product/${product._id}`}>
+										{" "}
 										<img
 											className="img-fluid"
 											src={product.image}
@@ -25,7 +26,6 @@ const Product = (props) => {
 										/>
 									</Link>
 								</Zoom>
-
 								<div className="card-body">
 									<h4 className="card-title">{product.name}</h4>
 									<p className="brand-name">
@@ -41,7 +41,6 @@ const Product = (props) => {
 									<button className="btn btn-outline-dark disabled">
 										{currencyFormat(product.price)}
 									</button>
-
 									<Link
 										className="btn btn-outline-danger ml-1"
 										to={`/product/${product._id}`}
@@ -57,5 +56,4 @@ const Product = (props) => {
 		</section>
 	);
 };
-
 export default Product;

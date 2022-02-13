9650b720d1c45cd5fed6110a1de593be05ee2650
Mahmoud Osman <mahmoudosman.som@gmail.com>
2022-02-12 22:20:44 -0600

    added ternary condition on orderHistoryScreen to display message

--- frontend/src/screens/OrderHistoryScreen.js
+++ frontend/src/screens/OrderHistoryScreen.js
@@ -18,6 +18,8 @@ const OrderHistoryScreen = (props) => {
 		dispatch(listOrderMine());
 	}, [dispatch]);
 
+ 
+
 	return (
 		<section className="order-history">
 			<div className="container">
@@ -29,62 +31,71 @@ const OrderHistoryScreen = (props) => {
 				) : (
 					<div className="row">
 						<div className="col-md-12">
-							<div class="table-responsive">
-								<table class="table table-hover table-striped">
-									<thead class="thead-dark">
-										<tr>
-											<th>ORDER ID</th>
-											<th>ORDER DATE</th>
-											<th>TOTAL PRICE</th>
-											<th>PAID</th>
-											<th>DELIVERED</th>
-											<th>ACTIONS</th>
-										</tr>
-									</thead>
-									<tbody>
-										{orders.map((order) => (
-											<tr key={order._id}>
-												<td>{order._id}</td>
-
-												<td>{order.createdAt.substring(0, 10)}</td>
-												<td>${order.totalPrice.toFixed(2)}</td>
-												<td>
-													{order.isPaid ? (
-														order.paidAt.substring(0, 10)
-													) : (
-														<span className="text-danger font-weight-bold">
-															NO
-														</span>
-													)}
-												</td>
-												<td>
-													{order.isDelivered ? (
-														order.deliveredAt.substring(0, 10)
-													) : (
-														<span className="text-danger font-weight-bold">
-															NO
-														</span>
-													)}
-												</td>
-												<td>
-													<button
-														type="button"
-														className="btn btn-success btn-sm"
-														onClick={() => {
-															props.history.push(`/order/${order._id}`);
-														}}
-													>
-														<i className="fa fa-eye" title="View details"></i>
-													</button>
-													<button className="btn btn-danger btn-sm ml-2">
-														<i className="fa fa-trash" title="Detlete"></i>
-													</button>
-												</td>
+							{orders.length <= 0 ? (
+								<div className="alert alert-danger">
+									<h3>
+										<i className="fa fa-warning"></i>&nbsp; You don't have any
+										order yet!
+									</h3>
+								</div>
+							) : (
+								<div class="table-responsive">
+									<table class="table table-hover table-striped">
+										<thead class="thead-dark">
+											<tr>
+												<th>ORDER ID</th>
+												<th>ORDER DATE</th>
+												<th>TOTAL PRICE</th>
+												<th>PAID</th>
+												<th>DELIVERED</th>
+												<th>ACTIONS</th>
 											</tr>
-										))}
-									</tbody>
-								</table>
-							</div>
+										</thead>
+										<tbody>
+											{orders.map((order) => (
+												<tr key={order._id}>
+													<td>{order._id}</td>
+
+													<td>{order.createdAt.substring(0, 10)}</td>
+													<td>${order.totalPrice.toFixed(2)}</td>
+													<td>
+														{order.isPaid ? (
+															order.paidAt.substring(0, 10)
+														) : (
+															<span className="text-danger font-weight-bold">
+																NO
+															</span>
+														)}
+													</td>
+													<td>
+														{order.isDelivered ? (
+															order.deliveredAt.substring(0, 10)
+														) : (
+															<span className="text-danger font-weight-bold">
+																NO
+															</span>
+														)}
+													</td>
+													<td>
+														<button
+															type="button"
+															className="btn btn-success btn-sm"
+															onClick={() => {
+																props.history.push(`/order/${order._id}`);
+															}}
+														>
+															<i className="fa fa-eye" title="View details"></i>
+														</button>
+														<button className="btn btn-danger btn-sm ml-2">
+															<i className="fa fa-trash" title="Detlete"></i>
+														</button>
+													</td>
+												</tr>
+											))}
+										</tbody>
+									</table>
+								</div>
+							)}
 						</div>
 					</div>
 				)}

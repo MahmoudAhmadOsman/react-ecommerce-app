518e9703b92be3eb624fa296b2cbe297a29f7479
Mahmoud Osman <mahmoudosman.som@gmail.com>
2022-02-24 00:01:31 -0600

    added fa-check-circle icon on form checkout

--- frontend/src/components/CheckoutSteps.js
+++ frontend/src/components/CheckoutSteps.js
@@ -2,15 +2,27 @@ import React from "react";
 
 const CheckoutSteps = (props) => {
   return (
-    <section className="checkout-main-steps container">
-      <div className="row checkout-steps">
-        <div className={props.step1 ? "active" : ""}>Sign In</div>
-        <div className={props.step2 ? "active" : ""}>Shipping</div>
-        <div className={props.step3 ? "active" : ""}>Payment</div>
-        <div className={props.step4 ? "active" : ""}>Place Order</div>
-      </div>
-    </section>
-  );
+		<section className="checkout-main-steps container">
+			<div className="row checkout-steps">
+				<div className={props.step1 ? "active fa fa-check-circle" : ""}>
+					&nbsp; Sign In
+				</div>
+				<div className={props.step2 ? "active fa fa-check-circle" : ""}>
+					{""} &nbsp; Shipping
+				</div>
+				<div className={props.step3 ? "active fa fa-check-circle" : ""}>
+					&nbsp;
+					{""}
+					Payment
+				</div>
+				<div className={props.step4 ? "active fa fa-check-circle" : ""}>
+					&nbsp;
+					{""}
+					Place Order
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default CheckoutSteps;
